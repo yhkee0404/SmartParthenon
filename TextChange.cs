77ab1de0b56bec37fe521aa84255be678d212e47
yhkee0404 <yhkee0404@gmail.com>
2018-02-02 16:49:34 +0900

    추가

---
+++ TextChange.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class TextChange : MonoBehaviour {
+    public Text testText;
+	void Start () {
+	}
+	void Update () {
+        testText.text = string.Format("프레임번호:{0}", Time.frameCount);
+	}
+}
